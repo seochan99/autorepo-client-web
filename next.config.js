286b59f74a733dfcd3c3780cac0f9da0b01867e9
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2024-12-02 04:12:30 +0900

    hotfix : doamin-s3

--- next.config.js
+++ next.config.js
@@ -1,13 +1,44 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
     images: {
-        domains: [process.env.NEXT_PUBLIC_S3_BUCKET_URL],
+        domains: process.env.NEXT_PUBLIC_S3_BUCKET_URL
+            ? [process.env.NEXT_PUBLIC_S3_BUCKET_URL]
+            : [''],
     },
     webpack(config) {
-        config.module.rules.push({
-            test: /\.svg$/,
-            use: ['@svgr/webpack'],
+        const fileLoaderRule = config.module?.rules?.find((rule) => {
+            return (
+                rule &&
+                typeof rule !== 'string' &&
+                rule.test instanceof RegExp &&
+                rule.test.test('.svg')
+            );
         });
+
+        if (fileLoaderRule && typeof fileLoaderRule !== 'string') {
+            config.module?.rules?.push(
+                {
+                    ...fileLoaderRule,
+                    test: /\.svg$/i,
+                    resourceQuery: /url/,
+                },
+                {
+                    test: /\.svg$/i,
+                    issuer: fileLoaderRule.issuer,
+                    resourceQuery: {
+                        not: [
+                            ...(fileLoaderRule.resourceQuery?.not || []),
+                            /url/,
+                        ],
+                    },
+                    use: ['@svgr/webpack'],
+                },
+            );
+
+            fileLoaderRule.exclude = /\.svg$/i;
+        }
+
         return config;
     },
 };
